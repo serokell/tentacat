82bd593b962f318e72700ee946b99c50725cbfa0
Lars Jellema <lars.jellema@gmail.com>
2018-09-28 16:01:43 +0200

    Bump httpoison and exvcr versions

--- mix.exs
+++ mix.exs
@@ -33,13 +33,13 @@ defmodule Tentacat.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 0.8"},
+      {:httpoison, "~> 1.2"},
       {:exjsx, "~> 4.0"},
       {:earmark, "~> 0.2.1", only: :dev},
       {:ex_doc, "~> 0.11.4", only: :dev},
       {:inch_ex, "~> 0.5", only: :dev},
       {:excoveralls, "~> 0.5", only: :test},
-      {:exvcr, "~> 0.9.1", only: :test},
+      {:exvcr, "~> 0.10", only: :test},
       {:meck, "~> 0.8.9", only: :test}
     ]
   end
